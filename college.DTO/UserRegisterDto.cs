e892893bfbbeaec304981df1982e70f29d348cf4
AhmedSeef <35418375+AhmedSeef@users.noreply.github.com>
2022-12-30 16:57:06 +0200

    Add Dto Project And User DTO

---
+++ college.DTO/UserRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using college.DTO.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace college.DTO
+{
+    public class UserRegisterDto : BaseDTO
+    {
+        [Required(ErrorMessage = "you must enter Name")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "you must enter the password")]
+        public string Password { get; set; }
+    }
+}
