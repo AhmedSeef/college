b2c3d81de20a07de23eeb238ba8730db75f999d1
AhmedSeef <35418375+AhmedSeef@users.noreply.github.com>
2022-12-30 16:58:19 +0200

    Add USer controller and register endpoint

---
+++ college.APIWEB/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using college.BL.Contract;
+using college.DTO;
+using college.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace college.APIWEB.Controllers
+{
+  
+    public class UserController : ApiController
+    {
+        private readonly IUserBL _userRepository;
+        public UserController(IUserBL userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        
+        public async Task<IHttpActionResult> Post(UserRegisterDto userRegister)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _userRepository.UserExist(userRegister.Username))
+                {
+                    return BadRequest("user name already exists");
+                }
+
+                var user = new User
+                {
+                    UserName = userRegister.Username                    
+                };
+               
+                return Ok(await _userRepository.Register(user, userRegister.Password));
+            }
+
+            return BadRequest(ModelState.ToString());
+        }
+    }
+}
