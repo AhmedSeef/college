9d31c6be66c0a2e6651d87c7432b473e7945e34d
AhmedSeef <35418375+AhmedSeef@users.noreply.github.com>
2023-01-01 12:42:15 +0200

    enable cors

--- college.APIWEB/App_Start/WebApiConfig.cs
+++ college.APIWEB/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace college.APIWEB
 {
@@ -11,7 +12,8 @@ namespace college.APIWEB
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
             config.MessageHandlers.Add(new TokenValidationHandler());
